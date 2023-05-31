288c686305dedfb7eaa517160035d702265977bb
Sikor08 <sikor08@gmail.com>
2023-05-31 15:09:38 +0300

    historyBottom

--- arrivDeskSwiper.js
+++ arrivDeskSwiper.js
@@ -1,30 +1,30 @@
 const arrivalsSliderList = document.querySelector('.arrivalsSliderList');
 
+
 const arrivalsSlider__left = document.querySelector('.arrivalsSlider__left');
 const arrivalsSlider__right = document.querySelector('.arrivalsSlider__right');
+
+const arrivalsSlidesQuantity = (arrivalsSliderList.children).length
 console.log((arrivalsSliderList.children).length) 
 
 let offset = 0;
 
 arrivalsSlider__left.addEventListener('click', () => {
     offset = offset + 30
-
-    if (offset >= 102) {
-        offset = -4
-    }
+    offset >= arrivalsSlidesQuantity* 34 -102? offset = -4 : offset;
     arrivalsSliderList.style.left = - offset + '%';
     offset += 4;
-    
-   
-    console.log(offset)
+    arrivalsSlider__right.removeAttribute('disabled', '')
 })
 
 arrivalsSlider__right.addEventListener('click', () => {
-    offset -= 38
-    arrivalsSliderList.style.left = - offset + '%';
-    offset += 4
+    if (offset == 0) {
+        arrivalsSlider__right.setAttribute('disabled', '')
+    } else {
+        arrivalsSlider__right.removeAttribute('disabled', '')
 
-    console.log(offset)
-
-   
+        offset -= 38
+        arrivalsSliderList.style.left = - offset + '%';
+        offset += 4
+    }
 })
